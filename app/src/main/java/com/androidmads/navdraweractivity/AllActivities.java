b2789531df2e473e2f3ab97b701f2f967611bbed
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-05-26 16:25:47 +0200

    Update AllActivities.java

--- app/src/main/java/com/androidmads/navdraweractivity/AllActivities.java
+++ app/src/main/java/com/androidmads/navdraweractivity/AllActivities.java
@@ -1,7 +1,9 @@
 package com.androidmads.navdraweractivity;
 
+import java.util.HashMap;
+
 public final class AllActivities {
-    public static final java.util.HashMap<Integer, Class> classMap = new java.util.HashMap<Integer, Class>();
+    public static final HashMap<Integer, Class> classMap = new HashMap<Integer, Class>();
     
     static {
         classMap.put(R.id.nav_activity1, FirstActivity.class);
